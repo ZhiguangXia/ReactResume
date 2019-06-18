787e9fa6a1dded94722f10587d3b715efc69106a
zhenzhencai <474760615@qq.com>
2019-06-18 19:39:07 +0800

    feat: 添加anti配置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button }  from 'antd';
 import './App.css';
 
 function App() {
@@ -8,16 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          测试中文字体
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Button type="primary">Button</Button>
+        </div>
+
       </header>
     </div>
   );
