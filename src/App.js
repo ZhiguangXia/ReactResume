79588fdf9b5461bd43c9fdafb6f141cf8bf8b06e
zhenzhencai <474760615@qq.com>
2019-06-20 10:43:12 +0800

    update: 构造主页布局

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import './App.less';
+import './App.css';
 
 function App() {
-  const DemoBox = props => <p className={'App-box'}>{props.children}</p>;
   return (
     <div className="App">
       <Row type="flex" justify="center" align="top">
-      <Col span={4}>
-        <DemoBox value={100}>col-4</DemoBox>
+      <Col xs={24} sm={24} lg={8}>
+        <div  className={'App-left'}>
+        </div>
       </Col>
-      <Col span={4}>
-        <DemoBox value={50}>col-4</DemoBox>
-      </Col>
-      <Col span={4}>
-        <DemoBox value={120}>col-4</DemoBox>
-      </Col>
-      <Col span={4}>
-        <DemoBox value={80}>col-4</DemoBox>
+      <Col xs={24} sm={24} lg={16}>
+        <div  className={'App-right'}>
+        </div>
       </Col>
     </Row>
 
